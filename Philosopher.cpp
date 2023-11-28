d2531765a428e3df3c40e1195bcf96a5648d3618
Gabriele <gabrielefiorentino2@gmail.com>
2023-11-28 15:04:05 +0100

    Add philosopher project

---
+++ Philosopher.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+using namespace std;
+
+const int philosopherNum = 5;
+
+mutex forks[philosopherNum];
+vector<thread> philosophers;
+
+
+void philosophersAction(int philosopherID) {
+
+    int leftFork = philosopherID;
+    int rightFork = (philosopherID + 1) % philosopherNum;
+
+    while (true)
+    {
+        //Thinking
+        cout << "Philosopher " << philosopherID << " is thinking." << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+
+        //Pick up forks
+        forks[leftFork].lock();
+        forks[rightFork].lock();
+
+        //Eating
+        cout << "Philosopher " << philosopherID << " is eating." << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+
+        forks[leftFork].unlock();
+        forks[rightFork].unlock();
+    }
+}
+
+
+int main()
+{
+    for (int i = 0; i < philosopherNum; i++) {
+        philosophers.emplace_back(philosophersAction, i);
+    }
+
+    for (auto& philosopher : philosophers) {
+        philosopher.join();
+    }
+
+    return 0;
+}
